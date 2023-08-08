eb136d4a15b0d99dc653a81143ca63d2086b72d1
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-08 05:54:14 +0000

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,14 +5,27 @@
  * list: linked list parameter
  * Return: 1 if cycle exists or 0 otherwise
  */
-
 int check_cycle(listint_t *list)
 {
-	listint_t *cur = list->next;
-	listint_t *pst_cur = cur->next;
+	listint_t *start, *fin;
 
-	if (cur != NULL && pst_cur != NULL)
-		return (1);
-	else
+	if (!list || !list->next)
+	{
 		return (0);
+	}
+	
+	start = list;
+	fin = list;
+	
+	while (fin != NULL && start->next != NULL && start->next->next != NULL)
+	{
+		start = start->next->next;
+		fin = fin->next;
+
+		if (start == fin)
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
