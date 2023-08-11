a5e69223d13165f364f6520d568ea588749cb85f
Richard Boakye Duah <richrichie784@gmail.com>
2023-08-10 23:55:54 -0700

    Adds 0-add.py file

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import add_0
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+
+    addResults = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, addResults))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,3 @@
+# 0x02-python-import_modules
+This directory is for tasks on python in importing modules and using them in
+other scripts or modules or files for good program structure.
