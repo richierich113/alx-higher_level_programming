90c40c9b549b8407b4cedd073d56c7b5cfa246d5
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-11 07:22:21 +0000

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-import add_0
-
 if __name__ == "__main__":
+    import add_0
     a = 1
     b = 2
-
-    addResults = add_0.add(a, b)
-    print("{} + {} = {}".format(a, b, addResults))
+    add_rslt = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, add_rslt))
