ff16695a6b5a6410da7d27c99fa14f705b08aea9
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-11 13:48:07 +0000

    Create 3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# a program that prints the result of the addition of all arguments
+from sys import argv
+
+if __name__ == "__main__":
+
+    arg_length = len(sys.argv) - 1
+    arg_list = argv[1:]
+    add_total = 0
+
+    # if no arg
+    if arg_length == 0:
+        print(add_total)
+    # if one arg
+    elif arg_length == 1:
+        total = add_total + int(argv[1])
+        print("{}".format(total))
+    # if multiple args
+    else:
+        for i in arg_list:
+            add_total += int(i)
+            print(f"{add_total:d}")
+        
