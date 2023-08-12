8a047802a7b57d9472079e9ad60ea22616c8ab31
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-12 01:59:15 +0000

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# a program that imports all functions from the file `calculator_1.py` and handles basic operations.
+# a program that imports all functions from the file `calculator_1.py`
+# and handles basic operations.
 from calculator_1 import add, sub, mul, div
 import sys
 
@@ -7,7 +8,7 @@ if __name__ == "__main__":
 
     arg_length = len(sys.argv) - 1
     arg_vals = sys.argv[1:4]
-    
+
     if arg_length < 3 or arg_length > 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
