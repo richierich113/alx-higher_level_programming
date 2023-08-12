c22b38be6a8c0ecf9bf566470c90f9b7c7c66b8a
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-12 00:36:02 +0000

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-
+# a program that prints all the names defined by the compiled module hidden_4.pyc
+#  that do not start with "__" in alpha order
 import hidden_4
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    # create empty list to append name to
+    final_names = []
+    # get the list od names in hidden_4 module and retrieve names as needed
     module_names = dir(hidden_4)
     for name in module_names:
         if name[:2] == "__":
             continue
-        print(f"{name}")
+        final_names.append(name)
+    # arrange final names in alpha order in a sorted list and print them
+    sorted_names = sorted(final_names)
+    for final_name in sorted_names:
+        print(final_name)
