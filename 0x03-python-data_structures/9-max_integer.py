183d5fefb2c33e023324cb37053be80392b03eed
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-14 17:51:29 +0000

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,48 +1,15 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    new_list = []
-    i = 0
-    for num in my_list[i::1]:
-        if i == 0:
-            new_list.append(num)
-        elif i == 1:
-            if num > my_list[i - 1]:
-                new_list.append(num)
-            else:
-                new_list.append(my_list[i - 1])
-        elif i > 1 and i <= (len(my_list) - 1):
-            if num > new_list[i - 1] and num > my_list[i - 1]:
-                new_list.append(num)
-            else:
-                new_list.append(new_list[i - 1])
+    if not my_list:
+        return None
 
-        i += 1
+    max_val = my_list[0]
+    for num in my_list[1:]:
+        if num > max_val:
+            max_val = num
 
-    if len(new_list) > 0:
-        if len(new_list) == 1:
-            return new_list[0]
-
-        new1_list = []
-        i = 0
-        for num in new_list[i::1]:
-            if i == 0:
-                new1_list.append(num)
-            elif i == 1:
-                if num > new_list[i - 1]:
-                    new1_list.append(num)
-                else:
-                    new1_list.append(new_list[i - 1])
-            elif i > 1 and i <= (len(new_list) - 1):
-                if num > new_list[i - 1] and num > new1_list[i - 1]:
-                    new1_list.append(num)
-                else:
-                    new1_list.append(new_list[i - 1])
-
-            i += 1
-
-        return new1_list[-1]
-
+    return max_val
 
 # for testing function
 if __name__ == "__main__":
