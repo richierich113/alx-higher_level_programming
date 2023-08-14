c99277e5eed8baecd304a4276fd300b79143a05b
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-14 18:37:39 +0000

    Create 11-delete_at.py

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list
+    else:
+        del my_list[idx]
+        return my_list
+
+
+# for testing function
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    new_list = delete_at(my_list, idx)
+    print(new_list)
+    print(my_list)
