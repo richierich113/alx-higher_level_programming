162cf78bd41afd0699ff41e8032792f73f96e8d3
Richard Boakye Duah <richrichie784@gmail.com>
2023-08-14 03:34:59 -0700

    Updates 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,7 +4,7 @@ def replace_in_list(my_list, idx, element):
     if idx >= 0:
         if idx > (len(my_list) - 1):
             return my_list
-        
+
         copy_list = my_list.copy()
         copy_list[idx] = element
         return copy_list
