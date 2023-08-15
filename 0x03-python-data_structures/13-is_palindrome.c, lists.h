473b314f728abc3a097bb9885d57146647912b1d
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-15 08:20:03 +0000

    Create 13-is_palindrome.c, lists.h

---
+++ 0x03-python-data_structures/13-is_palindrome.c, lists.h
@@ -0,0 +1,36 @@
+/**
+  * is_palindrome -  checks if a singly linked list is a palindrome.
+  * @head: head node of linked list
+  * Return: 1 if palindrome, 0 otherwise
+  */
+int is_palindrome(listint_t **head)
+{
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return 1;
+	}
+
+	listint_t *one_step = *head;
+	listint_t *two_steps = *head;
+
+	while (two_steps != NULL && two_steps->next != NULL)
+	{
+		one_step = one_step->next;
+		two_steps = two_steps->next->next;
+	}
+
+	one_step = reverse(one_step);
+	two_steps = *head;
+
+	while (one_step != NULL)
+	{
+		if (one_step->n != two_steps->n)
+		{
+			return 0; // Not a palindrome
+		}
+		one_step = one_step->next;
+		two_steps = two_steps->next;
+	}
+
+	return 1;
+}
