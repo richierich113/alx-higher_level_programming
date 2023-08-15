bf10d6aa01ca128e34aac56d6e94147507a77ec1
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-15 17:21:08 +0000

    Update reverse.c

--- 0x03-python-data_structures/reverse.c
+++ 0x03-python-data_structures/reverse.c
@@ -6,7 +6,7 @@
   * @head: head node of linked list to reverse
   * Return: reversed list
   */
-listint_t *reverse(listint_t *head)
+listint_t *reverse_list(listint_t *head)
 {
 	listint_t *prev = NULL;
 	listint_t *current = head;
