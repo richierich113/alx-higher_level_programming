fa903c33b32b0701efa8da125b4d797b3559e69d
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-16 20:41:43 +0000

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -6,8 +6,8 @@ def update_dictionary(a_dictionary, key, value):
 
 
 if __name__ == "__main__":
-    from 6-print_sorted_dictionary import print_sorted_dictionary
-
+    print_sorted_dictionary = __import__('6-print_sorted_dictionary')
+        .print_sorted_dictionary
     a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
     new_dict = update_dictionary(a_dictionary, 'language', "Python")
     print_sorted_dictionary(new_dict)
