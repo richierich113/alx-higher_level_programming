95e691e42065ec3df72144d0375bfeeeebaf0ae5
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-16 18:20:12 +0000

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    new_list = set_1.difference(set_2)
-    return new_list
+    only_set_1 = set_1.difference(set_2)
+    only_set_2 = set_2.difference(set_1)
+    new_set = only_set_1.union(only_set_2)
+    return new_set
+
 
 
 if __name__ == "__main__":
-    set_1 = {"Python", "C", "Javascript"}
-    set_2 = {"Bash", "C", "Ruby", "Perl"}
+    set_1 = { "Python", "C", "Javascript" }
+    set_2 = { "Bash", "C", "Ruby", "Perl" }
     od_set = only_diff_elements(set_1, set_2)
     print(sorted(list(od_set)))
