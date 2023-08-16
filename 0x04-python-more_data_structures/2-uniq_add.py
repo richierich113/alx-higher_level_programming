3e2b4191eeb4065bb72740a5ca8cca1501eb7e2b
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-16 17:37:03 +0000

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    if len(my_list) == 0:
-        return 0
-
-    new_list = [my_list[0]]
-    for num in my_list[1:]:
-        if num != new_list[-1]:
-            new_list.append(num)
-
-    add = sum(new_list)
-    return add
-
+    sum = 0
+    for i in set(my_list):
+        sum += i
+    return sum
 
 if __name__ == "__main__":
     my_list = [1, 2, 3, 1, 4, 2, 5]
