a36a633717fa8beb5b8fb7dc266eee9e48292534
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-16 20:00:33 +0000

    Update 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -16,5 +16,5 @@ def print_sorted_dictionary(a_dictionary):
 
 if __name__ == "__main__":
     a_dictionary = {'language': "C", 'Number': 89, 'track': "Low level",
-        'ids': [1, 2, 3]}
-            print_sorted_dictionary(a_dictionary)
+                   'ids': [1, 2, 3]}
+                       print_sorted_dictionary(a_dictionary)
