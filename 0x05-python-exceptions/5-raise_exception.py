cf79bd8fe3b615f08038b966cf5301ee02b30e8d
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-28 23:04:19 +0000

    Update 5-raise_exception.py

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -2,3 +2,10 @@
 
 def raise_exception():
     raise TypeError("Error message")
+
+
+if __name__ == "__main__":
+    try:
+        raise_exception()
+    except TypeError as te:
+        print("Exception raised")
