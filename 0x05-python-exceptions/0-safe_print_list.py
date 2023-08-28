a73b5c38ae5ff1145155e1846192dcf91d3de795
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-28 13:34:54 +0000

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
             print(f"IndexError: index {item} is out of range")
         except Exception as e:
             print(f"Error: {e}")
-    print("/n")
+    print("")
 
     return x
 
