0d107f310587691043369df3c60d547d9bc8cbac
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-28 18:17:34 +0000

    Create 2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    print_num = 0
+    for indx in range(0,x):
+        try:
+            item = my_list[indx]
+            print("{:d}".format(item))
+            print_num += 1
+        except IndexError as ie:
+            break
+        except Exception as e:
+            continue
+    return print_num
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    nb_print = safe_print_list_integers(my_list, 2)
+    print("nb_print: {:d}".format(nb_print))
+    my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+    nb_print = safe_print_list_integers(my_list, len(my_list))
+    print("nb_print: {:d}".format(nb_print))
+    nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+    print("nb_print: {:d}".format(nb_print))
+        
