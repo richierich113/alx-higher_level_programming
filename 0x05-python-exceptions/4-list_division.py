2b6c3bff74215c9d228e728b842ff56c86448b9e
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-28 22:47:17 +0000

    Update 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,19 +2,13 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     i = 0
-    new_list = list()
+    new_list = []
+    results = 0
 
     for indx_1 in range(0, list_length):
         try:
             element_1 = my_list_1[indx]
             element_2 = my_list_2[indx]
-
-            '''req_element_1 = isinstance(element_1(int, float))
-            req_element_2 = isinstance(element_2(int, float))
-            '''
-
-            if not isinstance(element_1, (int, float)) or not isinstance(element_2, (int, float)):
-                raise TypeError("wrong type")
             results = element_1 / element_2
         except IndexError:
             print("out of range")
