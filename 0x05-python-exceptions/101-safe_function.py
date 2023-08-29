814b7777594aa0d3c0b9f87330d062e7fe5821bd
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-29 05:51:48 +0000

    Create 101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as e:
+        return None
+        print(f"Exception: {e}", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    def my_div(a, b):
+        return a / b
+    result = safe_function(my_div, 10, 2)
+    print("result of my_div: {}".format(result))
+
+    result = safe_function(my_div, 10, 0)
+    print("result of my_div: {}".format(result))
+
+
+    def print_list(my_list, len):
+        i = 0
+        while i < len:
+            print(my_list[i])
+            i += 1
+        return len
+
+    result = safe_function(print_list, [1, 2, 3, 4], 10)
+    print("result of print_list: {}".format(result))
