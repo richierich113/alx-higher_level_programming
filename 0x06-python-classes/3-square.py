5b3363cc5e65b9a2e84edbc7f13c583b693dcf81
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-29 21:16:17 +0000

    Create 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""square docstring"""
+
+
+class Square:
+    """document for Square"""
+
+    def __init__(self, size=0):
+        """init method to initialize instance attributes
+        Args:
+            size (int): size of square parameter
+        """
+        self.__size = size
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+      def area(self):
+          """function for finding area of square
+          Returns:
+              int: area of square
+          """
+          return self.__size * self.__size
