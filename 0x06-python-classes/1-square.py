49b6c2d12fdbd83c40c1bba9015426bca5f5b682
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-29 20:08:45 +0000

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+class Square:
+
+    def __init__(self, size):
+        """init method to initialize instance attributes
+        Args:
+            size (int): size of square parameter
+        """
+        self.__size = size
