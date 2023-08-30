9aaef4b3bae968e0d9c5736dbc22dfccfa380086
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-30 06:24:52 +0000

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,86 +1,83 @@
 #!/usr/bin/python3
-"""square docstring"""
+"""My square module"""
 
 
 class Square:
-    """document for Square"""
+    """defines a square"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """init method to initialize instance attributes
+        """Create a Square
         Args:
-            size (int): size of square parameter
-            position (tuple): position of # in square
+            size: length of a side of Square
+            position: where the square is (coordinates)
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def __str__(self):
         self.my_print()
 
     @property
     def size(self):
-        """getter property for size instance attribute
-        Returns:
-            int: size of square
+        """"The propery of size as the len of a side of Square
+        Raises:
+            TypeError: if size != int
+            ValueError: if size < 0
         """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """setter property for size instance attribute
-        """
-        if isinstance(value, int):
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
-        else:
-            raise TypeError("size must be an integer")
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
     @property
     def position(self):
-        """getter property for size instance attribute, position
-        Returns:
-            int: tuple of # position"""
+        """property of the coordinates of this Square
+        Raises:
+            TypeError: if value != a tuple of 2 integers < 0
+        """
         return self.__position
 
     @position.setter
     def position(self, value):
-        """setter property for size instance attribute
+        """set the position of this Square
+        Args: value as a tuple of two positive integers
+        Raises:
+            TypeError: if value is not a tuple or any int in tuple < 0
         """
-        if isinstance(value, tuple) and len(value) == 2:
-            if isinstance(value[0], int) and isinstance(value[1], int):
-                if value[0] >= 0 and value[1] >= 0:
-                    self.__position = value
-        else:
-            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value, tuple):
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
 
     def area(self):
-        """function for finding area of square
-            Returns:
-                int: area of square
+        """Get the area of a Square
+        Returns: The size squared
         """
         return self.__size * self.__size
 
-    def print_position(self):
-        """use position in spaces
-        Returns:
-            tuple: spaces
-        """
-        positn = ""
+    def pos_print(self):
+        """returns the position in spaces"""
+        pos = ""
         if self.size == 0:
             return "\n"
-        for h in range(self.position[1]):
-            positn += "\n"
-        for h in range(self.size):
+        for w in range(self.position[1]):
+            pos += "\n"
+        for w in range(self.size):
             for i in range(self.position[0]):
-                positn += " "
+                pos += " "
             for j in range(self.size):
-                positn += "#"
-            positn += "\n"
-        return positn
+                pos += "#"
+            pos += "\n"
+        return pos
 
     def my_print(self):
-        """function for finding area of square
-        print the square in position
-        """
-        print(self.print_position(), end='')
+        """print the square in position"""
+        print(self.pos_print(), end='')
