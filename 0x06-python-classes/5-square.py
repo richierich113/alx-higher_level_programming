151d6a6569ed4160296967ce0996221422b810fb
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-30 00:20:11 +0000

    Create 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""square docstring"""
+
+
+class Square:
+    """document for Square"""
+
+    def __init__(self, size=0):
+        """init method to initialize instance attributes
+        Args:
+            size (int): size of square parameter
+        """
+        self.__size = size
+
+
+    @property
+    def size(self):
+        """getter property for size instance attribute
+        Returns:
+            int: size of square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """getter property for size instance attribute
+        Returns:
+            int: value of size
+        """
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """function for finding area of square
+            Returns:
+                int: area of square
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """function for finding area of square
+        """
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end='')
+                print("")
