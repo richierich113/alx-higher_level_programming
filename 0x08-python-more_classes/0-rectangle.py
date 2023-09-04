5b6e0f4122356078059c6c6ba10424454353c65c
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-04 16:34:30 +0000

    Update 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -7,3 +7,11 @@ class Rectangle:
     """empty Rectangle method   
     """
     pass
+
+
+if __name__ == "__main__":
+    Rectangle = __import__('0-rectangle').Rectangle
+
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
