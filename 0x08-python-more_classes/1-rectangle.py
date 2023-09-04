44dc36f6e7cebbc41387be9a435e7fb1bb82c60e
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-04 17:40:48 +0000

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
             width (int): width instnce attribute
             height (int): height instnce attribute
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
