de01e5703db80831b83413e3a11e3099e0b85874
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-04 16:37:33 +0000

    Update 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
- """module for Rectangle class  
- """
+"""module for Rectangle class  
+"""
 
 
 class Rectangle:
-    """empty Rectangle method   
+    """empty Rectangle method
     """
     pass
 
