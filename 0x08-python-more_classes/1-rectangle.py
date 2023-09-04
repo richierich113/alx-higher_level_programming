a8f7e627e9c0004c29d1bafc52a9a9edc2afda8d
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-04 17:15:25 +0000

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -61,7 +61,7 @@ class Rectangle:
 if __name__ == "__main__":
     my_rectangle = Rectangle(2, 4)
     print(my_rectangle.__dict__)
-    
+
     my_rectangle.width = 10
     my_rectangle.height = 3
     print(my_rectangle.__dict__)
