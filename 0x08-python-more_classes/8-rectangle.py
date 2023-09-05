ec54fb07f9e232a40a5bbb48fb07451aac8cce50
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-05 05:27:58 +0000

    Update 8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -93,12 +93,10 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_2.area() > rect_1.area():
-            return rect_2
-        elif rect_2.area() == rect_1.area():
+        if rect_2.area() <= rect_1.area():
             return rect_1
         else:
-            return rect_1
+            return rect_2
 
     def __str__(self):
         """__str__ method for class object when str()
