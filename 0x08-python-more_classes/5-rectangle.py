7c012f8be0c1fac6ecf3bb021cf2c26679413f30
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-05 04:25:03 +0000

    Update 5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -108,24 +108,12 @@ class Rectangle:
 
 
 if __name__ == "__main__":
+    Rectangle = __import__('5-rectangle').Rectangle
     my_rectangle = Rectangle(2, 4)
-    print(str(my_rectangle))
-    print("--")
-    print(my_rectangle)
-    print("--")
-    print(repr(my_rectangle))
-    print("--")
-    print(hex(id(my_rectangle)))
-    print("--")
-    # create new instance based on representation
-    new_rectangle = eval(repr(my_rectangle))
-    print(str(new_rectangle))
-    print("--")
-    print(new_rectangle)
-    print("--")
-    print(repr(new_rectangle))
-    print("--")
-    print(hex(id(new_rectangle)))
-    print("--")
-    print(new_rectangle is my_rectangle)
-    print(type(new_rectangle) is type(my_rectangle))
+    print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+    del my_rectangle
+    try:
+        print(my_rectangle)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
