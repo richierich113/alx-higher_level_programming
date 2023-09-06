8df577da539e3d2934824a364fb820bfc56cfb21
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-06 04:42:48 +0000

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -2,8 +2,3 @@
 def magic_string():
     magic_string.cnt = getattr(magic_string, 'cnt', 0) + 1
     return ", ".join(["BestSchool" for i in range(magic_string.cnt)])
-
-
-if __name__ == "__main__":
-    for i in range(10):
-        print(magic_string())
