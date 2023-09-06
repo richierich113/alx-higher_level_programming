8e9634bc5e66251eff34a16b996260caa913efd9
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-06 04:51:51 +0000

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -9,3 +9,12 @@ class LockedClass:
     attribute is called first_name.
     """
     __slots__ = ["first_name"]
+
+
+if __name__ == "__main__":
+    lc = LockedClass()
+    lc.first_name = "John"
+    try:
+        lc.last_name = "Snow"
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
