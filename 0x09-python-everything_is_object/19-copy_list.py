defdea5a9a5495291051a037f035293f59a7b78c
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-06 03:57:33 +0000

    Update 19-copy_list.py

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 def copy_list(l):
     return (l.copy())
-
-
-if __name__ == "__main__":
-    my_list = [1, 2, 3]
-    print(my_list)
-    new_list = copy_list(my_list)
-    print(my_list)
-    print(new_list)
-    print(new_list == my_list)
-    print(new_list is my_list)
