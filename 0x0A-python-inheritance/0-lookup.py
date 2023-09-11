f0fd5aac42be30cd63ae417740d53098407af0ab
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-11 19:57:23 +0000

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -16,14 +16,15 @@ def lookup(obj):
 
 if __name__ == "__main__":
     lookup = __import__('0-lookup').lookup
-
-class MyClass1(object):
+    
+    class MyClass1(object):
     pass
 
-class MyClass2(object):
+    class MyClass2(object):
     my_attr1 = 3
     def my_meth(self):
         pass
+
     print(lookup(MyClass1))
     print(lookup(MyClass2))
     print(lookup(int))
