01a4430b8e6dd10c0b73606f8cc6b4c488475100
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-11 19:54:53 +0000

    Create 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module contains a function def lookup(obj):
+"""
+
+
+def lookup(obj):
+    """Returns  the list of available attributes and
+    methods of an object.
+    Args:
+        obj (list): list parameter
+    Returns: list object
+    """
+    return (dir(obj))
+
+
+if __name__ == "__main__":
+    lookup = __import__('0-lookup').lookup
+
+class MyClass1(object):
+    pass
+
+class MyClass2(object):
+    my_attr1 = 3
+    def my_meth(self):
+        pass
+    print(lookup(MyClass1))
+    print(lookup(MyClass2))
+    print(lookup(int))
