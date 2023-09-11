df13c2a3dfa1ca898f6afb8b824f862978f390b3
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-11 21:15:47 +0000

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""This module inherits from the list class
+"""This module inherits a list class
 """
 
 
 class MyList(list):
+     """A class that prints a list"""
     def print_sorted(self):
         sorted_list = tuple(self)
         print(sorted_list)
