a12e42711a0826565c69b70ac4af8e9a0530ea73
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-12 18:06:11 +0000

    Update 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,18 +2,14 @@
 """module with a function that adds attributes to objects"""
 
 
-def add_attribute(obj, name, value):
+def add_attribute(obj, attrib, value):
     """Add a new attribute to an object if possible
     Raises
         TypeError: if you can't add a new attribute
     """
-    builtins = (str, int, complex, bool, float, list,
-                tuple, dict, set, frozenset, type, object)
-    for _type in builtins:
-        if type(obj) is _type:
-            raise TypeError("can't add new attribute")
-
-    object.__setattr__(obj, name, value)
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attrib, value)
 
 
 if __name__ == "__main__":
