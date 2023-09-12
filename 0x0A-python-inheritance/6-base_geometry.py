3bdada272bfbd3abdf1cd3630d9535f327319a4a
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-12 03:22:27 +0000

    Create 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module with class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """class with Public instance method:
+    def area(self)
+    """
+
+    def area(self):
+        """raises an Exception with a message 
+        """
+        raise Exception("area() is not implemented")
+
+
+if __name__ == "__main__":
+    bg = BaseGeometry()
+    try:
+        print(bg.area())
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
