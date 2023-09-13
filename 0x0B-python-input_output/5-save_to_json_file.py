6d83d8821c9e91d6dc6d4f44b16cc2f58dd44eb8
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-13 05:22:25 +0000

    Update 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     my_dict = {
         'id': 12,
         'name': "John",
-        'places': [ "San Francisco", "Tokyo" ],
+        'places': ["San Francisco", "Tokyo"],
         'is_active': True,
         'info': {
             'age': 36,
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     save_to_json_file(my_dict, filename)
     try:
         filename = "my_set.json"
-        my_set = { 132, 3 }
+        my_set = {132, 3}
         save_to_json_file(my_set, filename)
     except Exception as e:
         print("[{}] {}".format(e.__class__.__name__, e))
