9542b0dc3ce42e869848884c9c278656319eca0f
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-13 04:35:41 +0000

    Create 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module has a function that writes to a file"""
+
+
+def write_file(filename="", text=""):
+    """Writes to a UTF8 text file(UTF8)
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        return (file.write(text))
+
+
+if __name__ == "__main__":
+    nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+    print(nb_characters)
