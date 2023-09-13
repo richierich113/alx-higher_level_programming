bc6ddf335996c227cba7b96aa645a2406fcf0b41
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-13 05:58:08 +0000

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -8,3 +8,28 @@ def class_to_json(obj):
     integer and boolean) for JSON serialization of an object
     """
     return obj.__dict__
+
+
+if __name__ == "__main__":
+    """ My class module
+    """
+
+
+    class MyClass:
+        """ My class
+        """
+        def __init__(self, name):
+            self.name = name
+            self.number = 0
+
+        def __str__(self):
+            return "[MyClass] {} - {:d}".format(self.name, self.number)
+
+    m = MyClass("John")
+    m.number = 89
+    print(type(m))
+    print(m)
+
+    mj = class_to_json(m)
+    print(type(mj))
+    print(mj)
