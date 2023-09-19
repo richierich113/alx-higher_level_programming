0e590bcd0d4683f07b9097904f8098860f8d0381
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-19 03:26:57 +0000

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,11 +17,69 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
+
+    def area(self):
+        """returns the area value of the Rectangle instance.
+        """
+        return (self.width * self.height)
+
+    def display(self):
+        """prints in stdout the Rectangle instance with the
+        character #
+        """
+        for i in range(0, self.y):
+            print("")
+        for row in range(0, self.height):
+            for j in range(0, self.x):
+                print("")
+            for column in range(0, self.width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """overrides __str__ method
+        """
+        overrider = "[{}] ({}) {}/{} - {}/{}".format(self.__class__.__name__,
+                                                   self.id,
+                                                   self.x,
+                                                   self.y,
+                                                   self.width,
+                                                   self.height)
+        return overrider
+
+    def update(self, *args, **kwargs):
+        """assigns an argument to each attribute
+        """
+        if args is not None and len(args) > 0:
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.width = arg
+                elif i == 2:
+                    self.height = arg
+                elif i == 3:
+                    self.x = arg
+                elif i == 4:
+                    self.y = arg
+        elif kwargs is not None:
+            for (key, value) in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+
     @property
     def width(self):
         return self.__width
 
-     @width.setter
+    @width.setter
     def width(self, value):
         if type(value) != int:
             raise TypeError("width must be an integer")
@@ -33,7 +91,7 @@ class Rectangle(Base):
     def height(self):
         return self.__height
 
-     @height.setter
+    @height.setter
     def height(self, value):
         if type(value) != int:
             raise TypeError("height must be an integer")
