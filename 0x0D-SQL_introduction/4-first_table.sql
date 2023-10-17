c07ffd056b0a2a7ce782d5522a64a21c76794bee
Richard Duah Boakye <richrichie784@gmail.com>
2023-10-17 16:38:45 +0000

    Update 4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -2,4 +2,4 @@
 -- first_table description:
 -- id INT
 -- name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256))
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
