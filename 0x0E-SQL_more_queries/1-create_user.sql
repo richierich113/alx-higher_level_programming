c86527f85821b1772c68acd05bd9decf5a00add5
Richard Duah Boakye <richrichie784@gmail.com>
2023-10-18 15:39:58 +0000

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,4 +3,4 @@
 -- The user_0d_1 password will be set to user_0d_1_pwd
 -- If the user user_0d_1 already exists, script does not fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON '*'.'*' TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
