6e5b5d95a4020ddb2ab976369205e34b0fb6f9de
Richard Duah Boakye <richrichie784@gmail.com>
2023-10-18 22:53:08 +0000

    Update 16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,12 +4,7 @@
 -- Results must be sorted in ascending order by the show title and genre name
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT
-  tv_shows.title AS title,
-  tv_genres.name AS name
-  FROM tv_show_genres
-  RIGHT JOIN tv_shows
-  ON tv_show_genres.show_id = tv_shows.id
-  LEFT JOIN tv_genres
-  ON tv_genres.id = tv_show_genres.genre_id
-  ORDER BY title, name;
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_genres.name;
