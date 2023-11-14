631ab7bd37529880c581f38e9c773496dcb52381
Richard Duah Boakye <richrichie784@gmail.com>
2023-11-14 11:30:43 +0000

    Update 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -12,5 +12,5 @@ const argv = process.argv;
 const arg1 = argv[2];
 const arg2 = argv[3];
 
-//console.log(`${arg1} is ${arg2}`);
+// console.log(`${arg1} is ${arg2}`);
 console.log('arg1 ' + 'is ' + 'arg2');
