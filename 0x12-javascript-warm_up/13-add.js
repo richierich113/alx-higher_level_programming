d09f2667afb59bc0c466474d2bde0e2b9d9bb00b
Richard Duah Boakye <richrichie784@gmail.com>
2023-11-14 15:21:49 +0000

    Create 13-add.js

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/*
+A function that returns the addition of 2 integers.
+The function must be visible from outside
+The name of the function must be add
+You are not allowed to use var
+*/
+
+exports.add = function (a, b) {
+  return (a + b);
+};
