bbe73d5e163d67d6b02f99f5ababf66dd0e2b888
Richard Duah Boakye <richrichie784@gmail.com>
2023-11-14 10:35:26 +0000

    Update 0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -7,5 +7,5 @@ You must use console.log(...) to print all output
 You are not allowed to use var
 */
 
-const myVar = “JavaScript is amazing”;
+const myVar = 'JavaScript is amazing';
 console.log(myvar);
