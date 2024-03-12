58d679bf3e63975fffc9c2330a9f8acd415dee35
Richard Boakye Duah <richrichie784@gmail.com>
2024-03-11 22:35:00 -0700

    Adds 4-concat.js

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+/*
+A script that prints two arguments passed to it, in the following format: “ is ”
+You must use console.log(...) to print all output
+You are not allowed to use var
+*/
+
+const argv = process.argv;
+// Passed arguments start from index 2 (ie 0 1 2)
+
+const arg1 = argv[2];
+const arg2 = argv[3];
+
+// console.log(`${arg1} is ${arg2}`);
+console.log(arg1 + ' is ' + arg2);
