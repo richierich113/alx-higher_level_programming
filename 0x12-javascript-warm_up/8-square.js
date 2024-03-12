603b8c31d2136aead6c919e7078225c208bc8797
Richard Boakye Duah <richrichie784@gmail.com>
2024-03-11 22:52:55 -0700

    Adds 8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+/*
+A script that prints a square
+The first argument is the size of the square
+If the first argument can’t be converted to an integer, print “Missing size”
+You must use the character X to print the square
+You must use console.log(...) to print all output
+You are not allowed to use var
+You must use a loop (while, for, etc.)
+*/
+const argv = process.argv;
+
+if (argv.length < 3) {
+  console.log('Missing size');
+} else if (argv.length >= 3) {
+  const arg1 = argv[2];
+  const size = parseInt(arg1);
+  if (!isNaN(size)) {
+    const row = 'X'.repeat(size);
+    for (let j = 0; j < size; j++) {
+      console.log(row);
+    }
+  } else {
+    console.log('Missing size');
+  }
+}
