d96da674e1386564614e84b8f3703f92f5f350f1
Richard Duah Boakye <richrichie784@gmail.com>
2024-03-29 05:53:14 +0000

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -17,27 +17,29 @@ Please test your script in the sandbox provided, using the web server running
 on port 5000
 '''
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    from sys import argv
     import requests
-    import sys
-    url = "http://0.0.0.0:5000/search_user"
-    if len(sys.argv) > 1:
-        q = sys.argv[1]
-    else:
-        q = ""
-    query = {'q': q}
-    response = requests.post(url, data=query)
-    if response.status_code != requests.codes.ok or len(response.text) <= 0:
-        print('No result')
-        sys.exit()
-    else:
-        try:
-            my_obj = response.json()
-            if len(my_obj) == 0:
-                print('No result')
-                sys.exit()
-            my_id = my_obj.get('id')
-            my_name = my_obj.get('name')
-            print("[{}] {}".format(my_id, my_name))
-        except ValueError as invalid_json:
-            print('Not a valid JSON')
+
+    if len(argv) > 1:
+        letter = argv[1]
+    elif len(argv) == 1:
+        letter = ''
+
+    headers = {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": "python-requests/2.27.1"
+    }
+    data = {'q': letter}
+    url = 'http://0.0.0.0:5000/search_user'
+    res = requests.post(url, headers=headers, data=data)
+    try:
+        json = res.json()
+        if json != {}:
+            print(f"[{json['id']}] {json['name']}")
+        elif json == {}:
+            print('No result')
+    except Exception:
+        print('Not a valid JSON')
