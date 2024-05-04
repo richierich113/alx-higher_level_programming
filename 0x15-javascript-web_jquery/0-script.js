b3fb8dbbde0eb92580d1c7d2bbcc50656487bae0
Richard Boakye Duah <richrichie784@gmail.com>
2024-05-04 04:52:42 -0700

    Creates 0-main.html, 0-script.js & README.md files

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,8 @@
+/*
+Write a JavaScript script that updates the text color of the <header> element to red (#FF0000):
+
+You must use document.querySelector to select the HTML tag
+You can’t use the JQuery API
+*/
+
+document.querySelector('header').style.color = '#FF0000';
