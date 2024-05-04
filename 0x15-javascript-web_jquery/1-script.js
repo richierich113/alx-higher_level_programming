178f2927e0196a58128ec3e1402b9ea9df508ec8
Richard Duah Boakye <richrichie784@gmail.com>
2024-05-04 12:30:16 +0000

    Creates 1-script.js - Use of JQuery

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,18 @@
+/*
+Write a JavaScript script that updates the text color of the <header> element to red (#FF0000):
+
+You can’t use document.querySelector to select the HTML tag
+You must use the JQuery API
+*/
+
+//This ensures that the code inside the function is executed
+//once the HTML document is fully loaded and ready for interaction.
+$(function() {
+    $('header').css('color', '#FF0000');
+})
+
+//This is the extended form of the code above
+/*$(document).ready(function() {
+  $('header').css('color', '#FF0000');
+});
+*/
